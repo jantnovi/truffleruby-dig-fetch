958aefd3ae19170bbd5ad6d005bddf2f046f0c80
Julie Antunovic <julie.antunovic@shopify.com>
2021-03-08 23:16:01 -0500

    Add benchmarking for dig, fetch chaining, and dig_fetch

---
+++ benchmark/mri_benchmark_3.rb
@@ -0,0 +1,21 @@
+require 'benchmark/ips'
+
+thing = {
+    :foo => {
+        :bar => {       
+            :foobar => "foobar"
+        }
+    }
+}
+
+Benchmark.ips do |x|
+  x.iterations = 3
+
+  x.report("digging") do
+    thing.dig(:foo, :bar, :foobar)
+  end
+
+  x.report("fetch chaining") do
+    thing.fetch(:foo).fetch(:bar).fetch(:foobar)
+  end
+end
